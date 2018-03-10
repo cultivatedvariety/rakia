18360d4350858b92e5dd07e4185ec9f4f1a953bc
cultivatedvariety <rob.cherry@gmail.com>
2018-03-10 20:07:23 +0000

    ...

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,17 +8,17 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            SliceIndex sliceIndex = null;
             try
             {
-                SliceIndex sliceIndex = new SliceIndex(@"/Users/rob/Documents/work/rakia/ConsoleApp/slice.idx");
-                byte[] key1 = Encoding.UTF8.GetBytes("key1");
-                byte[] key2 = Encoding.UTF8.GetBytes("key1");
-                Console.WriteLine(key1 == key2);
+                
+                
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
+            sliceIndex?.Close();
             Console.WriteLine("\n\nDone");
             Console.ReadLine();
         }
